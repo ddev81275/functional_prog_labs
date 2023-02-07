ce513c23c7e0162cba5760532ae9d7604694a9f8
almasmendybayev <almas_mendybayev@epam.com>
2023-02-07 22:49:56 +0600

    task 2,3(some issue with task 2)

--- lab_3/tasks.py
+++ lab_3/tasks.py
@@ -1,8 +1,9 @@
-# 1) Написать программу используя цикл for и while.
 from datetime import date
 import calendar
+import random
 
 
+# 1) Написать программу используя цикл for и while.
 def months_between_current_and_last():
     current_month = date.today().month  # Feb
     print("current month is : "+calendar.month_name[current_month])
@@ -14,4 +15,27 @@ def months_between_current_and_last():
         print(calendar.month_name[item], end=" ")
 
 
-months_between_current_and_last()
+# 2) используя функцию range() сделать список.
+# в функцию range() введите данные с разными типами и выведите на экран в разных примерах.
+def create_int_list():
+    ints = range(0, 10, 1)
+    for item in ints:
+        print(item, end=" ")
+
+# 3) Используйте функции randint() randrange() random() enumerate()
+# в своей программе
+def _random_():
+    _randint = random.randint(0, 9)
+    _randrange = random.randrange(10)
+    _random = random.random()
+    _list = ["abc", 34, True, 40, "male"]
+
+
+    print(list(enumerate(_list)))
+    print(f"randint(0, 9) => {_randint}")
+    print(f"random.randrange({10}) => {_randrange}")
+    print(f"random.random() => {_random}")
+
+# months_between_current_and_last()
+# create_int_list()
+_random_()
