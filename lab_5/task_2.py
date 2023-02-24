07a88ba805881a33e4cf50e7c2d370be59715b96
almasmendybayev <almas_mendybayev@epam.com>
2023-02-24 23:03:55 +0600

    minor fix

--- lab_5/task_2.py
+++ lab_5/task_2.py
@@ -2,6 +2,20 @@
 # подготовьте	 список	 предметов	 и	 оценок	 учащихся. Когда вы	 вводите
 # имя	учащегося,	то	должны отображаться оценки	этого	учащегося.
 
-# lessons ["Math", "Physics", "Bio", "Geo", "History"]
-# marks [2, 3, 4, 5]
-# student ["Tom.J", "Alex.H", "Adam.A", "Anny.T"]
+_alex = [["Alex"], ["Math", [2, 3, 5, 4]], ["History", [3, 4, 4, 4]]]
+
+_tom  = [["Tom"], ["Math", [4, 4, 3, 4]], ["History", [3, 3, 4, 5]]]
+
+_anna = [["Anna"], ["Bio", [5, 4, 3, 3]], ["Geo", [3, 2, 5, 5]]]
+
+common = []
+common.append(_alex)
+common.append(_tom)
+common.append(_anna)
+
+print(common)
+print("Input name: ", end=" ")
+name = input()
+for item in common:
+    if name.lower() == item[0][0].lower():
+        print(item)
